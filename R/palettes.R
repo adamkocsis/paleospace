48dd99c86f65f00c90191576c3756f6662eb45fe
adamkocsis <adam.t.kocsis@gmail.com>
2024-01-17 18:08:04 +0100

    Added ramp calculations.

--- R/palettes.R
+++ R/palettes.R
@@ -21,3 +21,75 @@ NULL
 #' 
 #' @export 
 gradinv <- grDevices::colorRampPalette(c("#33358a", "#76acce", "#fff99a",  "#e22c28", "#690720"))
+
+
+#' Calibrated topographic color palettes
+#'
+#' The object contains \code{data.frame}-class objects.
+#'
+#' @format A \code{list} with 2 \code{data.frame}s:
+#' \describe{
+#' \item{\code{jakarta}: The "Jakarta" theme.}
+#' \item{\code{havanna2}: The "Havanna-2" theme.}
+#' \item{\code{tokio1}: The "tokio-1" theme.}
+#' \item{\code{zagreb}: The "zagreb" theme.}
+#' }
+#' @usage data(topos)
+#' 
+"topos"
+
+
+#' Create color ramp palette from fixed color objects
+#'
+#' Function to create ramp palettes from fixed color positions
+#'
+#' @param x A \code{data.frame} object with two columns: \code{color} for hexadecimal color values, \code{z} for their position.
+#' @param n Single integer number
+#'
+#' @return A \code{list}-class object, with three elements: \code{$col} hexadecimal color values, \code{$mid} z-values of midpoints (one for every color, \code{$breaks} separator borders between color values.
+#' @export
+expandramp <- function(x, n){
+	# separate data
+	color <- x$color
+	z <- x$z
+
+	# generate sequence between minimum and maximum
+	centers <- seq(min(z), max(z), length.out=n)
+	# difference between the bins
+	di <- diff(centers)[1]
+
+	# the breaks (ready for return)
+	breaks <- c(centers-di/2, centers[length(centers)]+di/2)
+
+	# find the closest midpoint to every given z value
+	col <- rep(NA, n)
+
+	# where are the given color values
+	index <- rep(NA, length(z))
+	for(i in 1:length(z)){
+		# differences
+		absDiffs <- abs(z[i] - centers)
+		index[i] <- which(min(absDiffs)==absDiffs)[1]
+	}
+
+	# construct the ramps
+	for(i in 2:length(index)){
+		# the indices where this should go
+		fit <- index[i-1]:index[i]
+		# generate ramp generator function
+		thisChunkFunc <- grDevices::colorRampPalette(c(color[i-1], color[i]))
+		# construct this part of the ramp
+		colorSubset <- thisChunkFunc(length(fit))
+
+		# fit this bit into the complete palette
+		col[fit] <- colorSubset
+	}
+	
+	y<- list(
+		col=col,
+		breaks=breaks,
+		mid=centers
+	)
+
+	return(y)
+}
